bd737f376c6d75461adf22b03ef4ee0facbbd373
Minh Nguyễn <mxn@1ec5.org>
2021-02-23 10:52:20 -0800

    Removed dependency on MapboxAccounts

--- Sources/CMapboxCoreNavigation/include/MBXAccounts+CoreNavigationAdditions.h
+++ Sources/CMapboxCoreNavigation/include/MBXAccounts+CoreNavigationAdditions.h
@@ -1,8 +1,10 @@
-#import <MapboxAccounts/MapboxAccounts.h>
+#import <MapboxCommon/MapboxCommon.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface MBXAccounts (CoreNavigationAdditions)
+@interface MBXAccounts : NSObject
+
+@property (class, nonatomic, readonly, nullable) NSString *serviceSkuToken;
 
 @end
 
--- Sources/CMapboxCoreNavigation/include/MBXAccounts+CoreNavigationAdditions.m
+++ Sources/CMapboxCoreNavigation/include/MBXAccounts+CoreNavigationAdditions.m
@@ -3,18 +3,19 @@
 static NSString * const MBXNavigationBillingMethodUser = @"user";
 static NSString * const MBXNavigationBillingMethodRequest = @"request";
 
-@implementation MBXAccounts (CoreNavigationAdditions)
+@implementation MBXAccounts
 
-+ (void)load {
++ (nullable NSString *)serviceSkuToken {
     NSString *billingMethodValue = [NSBundle.mainBundle objectForInfoDictionaryKey:@"MBXNavigationBillingMethod"];
     if (!billingMethodValue.length || [billingMethodValue isEqualToString:MBXNavigationBillingMethodUser]) {
-        [MBXAccounts activateSKUID:MBXAccountsSKUIDNavigationUser];
+        return [MBXTokenGenerator getSKUTokenForIdentifier:MBXSKUIdentifierNavigationMAUS withError:NULL];
     } else if (![billingMethodValue isEqualToString:MBXNavigationBillingMethodRequest]) {
         // Billing method is not the default in the absence of a SKU ID.
         [NSException raise:@"MBXInvalidNavigationBillingMethod"
                     format:@"Unrecognized billing method %@. Valid billing methods are: %@, %@.",
          billingMethodValue, MBXNavigationBillingMethodUser, MBXNavigationBillingMethodRequest];
     }
+    return nil;
 }
 
 @end
